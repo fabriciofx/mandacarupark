b8c659ab20f0292df2f893f606571797ec42798d
Fabrício Barros Cabral <fabriciofx@gmail.com>
2022-07-26 20:23:54 -0300

    (#15) Continuando a substituir código por Select

--- src/main/java/br/edu/ifpe/ead/si/mandacarupark/sql/SqlSaida.java
+++ src/main/java/br/edu/ifpe/ead/si/mandacarupark/sql/SqlSaida.java
@@ -26,9 +26,8 @@ package br.edu.ifpe.ead.si.mandacarupark.sql;
 import br.edu.ifpe.ead.si.mandacarupark.Placa;
 import br.edu.ifpe.ead.si.mandacarupark.Saida;
 import br.edu.ifpe.ead.si.mandacarupark.Uuid;
+import br.edu.ifpe.ead.si.mandacarupark.db.Select;
 import br.edu.ifpe.ead.si.mandacarupark.db.Session;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -49,28 +48,26 @@ public class SqlSaida implements Saida {
 
     @Override
     public Placa placa() {
-        Placa placa = null;
         String sql = String.format(
             "SELECT placa FROM saida WHERE id = '%s'",
             this.id
         );
-        try (Connection conn = this.session.connection()) {
-            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-                try (ResultSet rset = stmt.executeQuery()) {
-                    if (rset.next()) {
-                        placa = new Placa(rset.getString(1));
-                    }
-                }
+        try {
+            Placa placa ;
+            ResultSet rset = new Select(this.session, sql).result();
+            if (rset.next()) {
+                placa = new Placa(rset.getString(1));
+            } else {
+                throw new RuntimeException("Placa inexistente ou inválida!");
             }
+            return placa;
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
-        return placa;
     }
 
     @Override
     public LocalDateTime dataHora() {
-        LocalDateTime dataHora;
         DateTimeFormatter formato = DateTimeFormatter.ofPattern(
             "yyyy-MM-dd HH:mm:ss.SSSX"
         );
@@ -78,18 +75,19 @@ public class SqlSaida implements Saida {
             "SELECT datahora FROM saida WHERE id = '%s'",
             this.id
         );
-        try (Connection conn = this.session.connection()) {
-            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-                try (ResultSet rset = stmt.executeQuery()) {
-                    dataHora = LocalDateTime.parse(
-                        rset.getString(1),
-                        formato
-                    );
-                }
+        try {
+            LocalDateTime dataHora;
+            ResultSet rset = new Select(this.session, sql).result();
+            if (rset.next()) {
+                dataHora = LocalDateTime.parse(rset.getString(1), formato);
+            } else {
+                throw new RuntimeException(
+                    "Data/Hora inexistente ou inválida!"
+                );
             }
+            return dataHora;
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
-        return dataHora;
     }
 }
