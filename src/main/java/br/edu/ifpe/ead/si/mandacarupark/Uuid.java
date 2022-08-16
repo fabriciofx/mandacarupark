fa08e1985967f4ddd31af6bfef27e840a33bb12b
Fabrício Barros Cabral <fabriciofx@gmail.com>
2022-08-15 22:12:47 -0300

    (#70) Uuid agora usa hífen e minúsculas

--- src/main/java/br/edu/ifpe/ead/si/mandacarupark/Uuid.java
+++ src/main/java/br/edu/ifpe/ead/si/mandacarupark/Uuid.java
@@ -43,7 +43,7 @@ public final class Uuid {
      * Ctor.
      */
     public Uuid() {
-        this(UUID.randomUUID().toString().toUpperCase().replaceAll("-", ""));
+        this(UUID.randomUUID().toString());
     }
 
     /**
