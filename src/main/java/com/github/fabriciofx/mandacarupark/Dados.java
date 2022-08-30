6c2f7e250fa28faacc11d75abe928191679c1311
Fabrício Barros Cabral <fabriciofx@gmail.com>
2022-08-30 18:33:07 -0300

    (#103) Corrige bug que não permitia executar o Dados::com()

--- src/main/java/com/github/fabriciofx/mandacarupark/Dados.java
+++ src/main/java/com/github/fabriciofx/mandacarupark/Dados.java
@@ -23,6 +23,7 @@
  */
 package com.github.fabriciofx.mandacarupark;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Dados {
@@ -50,28 +51,20 @@ public class Dados {
         final String chave10,
         final Object valor10
     ) {
-        this(Map.of(
-            chave1,
-            valor1,
-            chave2,
-            valor2,
-            chave3,
-            valor3,
-            chave4,
-            valor4,
-            chave5,
-            valor5,
-            chave6,
-            valor6,
-            chave7,
-            valor7,
-            chave8,
-            valor8,
-            chave9,
-            valor9,
-            chave10,
-            valor10
-        ));
+        this(
+            Dados.of(
+                new MapEntry<>(chave1, valor1),
+                new MapEntry<>(chave2, valor2),
+                new MapEntry<>(chave3, valor3),
+                new MapEntry<>(chave4, valor4),
+                new MapEntry<>(chave5, valor5),
+                new MapEntry<>(chave6, valor6),
+                new MapEntry<>(chave7, valor7),
+                new MapEntry<>(chave8, valor8),
+                new MapEntry<>(chave9, valor9),
+                new MapEntry<>(chave10, valor10)
+            )
+        );
     }
 
     public Dados(
@@ -94,26 +87,19 @@ public class Dados {
         final String chave9,
         final Object valor9
     ) {
-        this(Map.of(
-            chave1,
-            valor1,
-            chave2,
-            valor2,
-            chave3,
-            valor3,
-            chave4,
-            valor4,
-            chave5,
-            valor5,
-            chave6,
-            valor6,
-            chave7,
-            valor7,
-            chave8,
-            valor8,
-            chave9,
-            valor9
-        ));
+        this(
+            Dados.of(
+                new MapEntry<>(chave1, valor1),
+                new MapEntry<>(chave2, valor2),
+                new MapEntry<>(chave3, valor3),
+                new MapEntry<>(chave4, valor4),
+                new MapEntry<>(chave5, valor5),
+                new MapEntry<>(chave6, valor6),
+                new MapEntry<>(chave7, valor7),
+                new MapEntry<>(chave8, valor8),
+                new MapEntry<>(chave9, valor9)
+            )
+        );
     }
 
     public Dados(
@@ -134,24 +120,18 @@ public class Dados {
         final String chave8,
         final Object valor8
     ) {
-        this(Map.of(
-            chave1,
-            valor1,
-            chave2,
-            valor2,
-            chave3,
-            valor3,
-            chave4,
-            valor4,
-            chave5,
-            valor5,
-            chave6,
-            valor6,
-            chave7,
-            valor7,
-            chave8,
-            valor8
-        ));
+        this(
+            Dados.of(
+                new MapEntry<>(chave1, valor1),
+                new MapEntry<>(chave2, valor2),
+                new MapEntry<>(chave3, valor3),
+                new MapEntry<>(chave4, valor4),
+                new MapEntry<>(chave5, valor5),
+                new MapEntry<>(chave6, valor6),
+                new MapEntry<>(chave7, valor7),
+                new MapEntry<>(chave8, valor8)
+            )
+        );
     }
 
     public Dados(
@@ -170,22 +150,17 @@ public class Dados {
         final String chave7,
         final Object valor7
     ) {
-        this(Map.of(
-            chave1,
-            valor1,
-            chave2,
-            valor2,
-            chave3,
-            valor3,
-            chave4,
-            valor4,
-            chave5,
-            valor5,
-            chave6,
-            valor6,
-            chave7,
-            valor7
-        ));
+        this(
+            Dados.of(
+                new MapEntry<>(chave1, valor1),
+                new MapEntry<>(chave2, valor2),
+                new MapEntry<>(chave3, valor3),
+                new MapEntry<>(chave4, valor4),
+                new MapEntry<>(chave5, valor5),
+                new MapEntry<>(chave6, valor6),
+                new MapEntry<>(chave7, valor7)
+            )
+        );
     }
 
     public Dados(
@@ -202,20 +177,16 @@ public class Dados {
         final String chave6,
         final Object valor6
     ) {
-        this(Map.of(
-            chave1,
-            valor1,
-            chave2,
-            valor2,
-            chave3,
-            valor3,
-            chave4,
-            valor4,
-            chave5,
-            valor5,
-            chave6,
-            valor6
-        ));
+        this(
+            Dados.of(
+                new MapEntry<>(chave1, valor1),
+                new MapEntry<>(chave2, valor2),
+                new MapEntry<>(chave3, valor3),
+                new MapEntry<>(chave4, valor4),
+                new MapEntry<>(chave5, valor5),
+                new MapEntry<>(chave6, valor6)
+            )
+        );
     }
 
     public Dados(
@@ -230,18 +201,15 @@ public class Dados {
         final String chave5,
         final Object valor5
     ) {
-        this(Map.of(
-            chave1,
-            valor1,
-            chave2,
-            valor2,
-            chave3,
-            valor3,
-            chave4,
-            valor4,
-            chave5,
-            valor5
-        ));
+        this(
+            Dados.of(
+                new MapEntry<>(chave1, valor1),
+                new MapEntry<>(chave2, valor2),
+                new MapEntry<>(chave3, valor3),
+                new MapEntry<>(chave4, valor4),
+                new MapEntry<>(chave5, valor5)
+            )
+        );
     }
 
     public Dados(
@@ -254,16 +222,14 @@ public class Dados {
         final String chave4,
         final Object valor4
     ) {
-        this(Map.of(
-            chave1,
-            valor1,
-            chave2,
-            valor2,
-            chave3,
-            valor3,
-            chave4,
-            valor4
-        ));
+        this(
+            Dados.of(
+                new MapEntry<>(chave1, valor1),
+                new MapEntry<>(chave2, valor2),
+                new MapEntry<>(chave3, valor3),
+                new MapEntry<>(chave4, valor4)
+            )
+        );
     }
 
     public Dados(
@@ -274,7 +240,13 @@ public class Dados {
         final String chave3,
         final Object valor3
     ) {
-        this(Map.of(chave1, valor1, chave2, valor2, chave3, valor3));
+        this(
+            Dados.of(
+                new MapEntry<>(chave1, valor1),
+                new MapEntry<>(chave2, valor2),
+                new MapEntry<>(chave3, valor3)
+            )
+        );
     }
 
     public Dados(
@@ -283,11 +255,16 @@ public class Dados {
         final String chave2,
         final Object valor2
     ) {
-        this(Map.of(chave1, valor1, chave2, valor2));
+        this(
+            Dados.of(
+                new MapEntry<>(chave1, valor1),
+                new MapEntry<>(chave2, valor2)
+            )
+        );
     }
 
     public Dados(final String chave1, final Object valor1) {
-        this(Map.of(chave1, valor1));
+        this(Dados.of(new MapEntry<>(chave1, valor1)));
     }
 
     public Dados(final Map<String, Object> itens) {
@@ -306,5 +283,41 @@ public class Dados {
     @Override
     public String toString() {
         return this.itens.toString();
+    }
+
+    private static class MapEntry<K, V> implements Map.Entry<K, V> {
+        private final K key;
+        private final V value;
+
+        public MapEntry(final K key, final V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public K getKey() {
+            return this.key;
+        }
+
+        @Override
+        public V getValue() {
+            return this.value;
+        }
+
+        @Override
+        public V setValue(V value) {
+            throw new UnsupportedOperationException("MapEntry::setValue()");
+        }
+    }
+
+    @SafeVarargs
+    private static <X, Y> Map<X, Y> of(
+        final Map.Entry<? extends X, ? extends Y> ...entries
+    ) {
+        final Map<X, Y> map = new HashMap<>(0);
+        for (final Map.Entry<? extends X, ? extends Y> entry : entries) {
+            map.put(entry.getKey(), entry.getValue());
+        }
+        return map;
     }
 }
