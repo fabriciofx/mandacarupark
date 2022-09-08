cac8f8a8a492a614424999cd88b38b7919d28026
Fabrício Barros Cabral <fabriciofx@gmail.com>
2022-09-07 21:31:06 -0300

    (#136) Adiciona SimEstacionamento, um simulador

---
+++ src/test/java/com/github/fabriciofx/mandacarupark/estacionamento/SimEstacionamento.java
@@ -0,0 +1,96 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (C) 2022 Fabrício Barros Cabral
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.github.fabriciofx.mandacarupark.estacionamento;
+
+import com.github.fabriciofx.mandacarupark.Contas;
+import com.github.fabriciofx.mandacarupark.DataHora;
+import com.github.fabriciofx.mandacarupark.Dinheiro;
+import com.github.fabriciofx.mandacarupark.Entradas;
+import com.github.fabriciofx.mandacarupark.Estacionamento;
+import com.github.fabriciofx.mandacarupark.Pagamentos;
+import com.github.fabriciofx.mandacarupark.Placa;
+import com.github.fabriciofx.mandacarupark.Saidas;
+import com.github.fabriciofx.mandacarupark.Ticket;
+import com.github.fabriciofx.mandacarupark.conta.DomingoGratis;
+import com.github.fabriciofx.mandacarupark.conta.Tolerancia;
+import com.github.fabriciofx.mandacarupark.conta.ValorFixo;
+import com.github.fabriciofx.mandacarupark.datahora.DataHoraOf;
+import com.github.fabriciofx.mandacarupark.datahora.DataHoraRandom;
+import com.github.fabriciofx.mandacarupark.entradas.EntradasFake;
+import com.github.fabriciofx.mandacarupark.pagamentos.PagamentosFake;
+import com.github.fabriciofx.mandacarupark.placa.PlacaOf;
+import com.github.fabriciofx.mandacarupark.saidas.SaidasFake;
+import com.github.fabriciofx.mandacarupark.ticket.imagem.ImagemParaArquivo;
+import org.junit.Test;
+import java.io.File;
+import java.time.LocalDateTime;
+import java.util.concurrent.ThreadLocalRandom;
+
+public final class SimEstacionamento {
+    @Test
+    public void simulacao() {
+        // 1. Monta-se o ambiente do estacionamento
+        final Entradas entradas = new EntradasFake();
+        final Saidas saidas = new SaidasFake();
+        final Pagamentos pagamentos = new PagamentosFake();
+        final Estacionamento estacionamento = new EstacionamentoFake(
+            entradas,
+            saidas,
+            pagamentos,
+            new Contas(
+                new DomingoGratis(),
+                new Tolerancia(),
+                new ValorFixo(new Dinheiro("5.00"))
+            )
+        );
+        for (int idx = 0; idx < 1000000; idx++) {
+            final Placa placa = new PlacaOf();
+            final DataHora dataHora = new DataHoraRandom(
+                LocalDateTime.of(2022, 8, 2, 8, 0, 0),
+                LocalDateTime.of(2022, 8, 2, 22, 0, 0)
+            );
+            // 2. Um veículo aleatório, de um tempo aleatório T dá entrada no
+            //    estacionamento e recebe um ticket
+            final Ticket ticket = estacionamento.entrada(placa, dataHora);
+            // 3. O proprietário do veículo guarda o ticket
+            //new ImagemParaArquivo(ticket.imagem(), "ticket.png").salva();
+            // 4. Depois de um tempo aletório T + N, o proprietário paga o
+            //    estacionamento
+            final int N = ThreadLocalRandom.current().nextInt(10, 841);
+            final Ticket ticketValidado = estacionamento.pagamento(
+                ticket,
+                new DataHoraOf(dataHora.dateTime().plusMinutes(N))
+            );
+            // 5. Depois de um tempo aletório T + N + K, o proprietário deixa o
+            //    estacionamento
+            final int K = ThreadLocalRandom.current().nextInt(N, 841);
+            estacionamento.saida(
+                ticketValidado,
+                placa,
+                new DataHoraOf(dataHora.dateTime().plusMinutes(N + K))
+            );
+        }
+        //new File("ticket.png").delete();
+    }
+}
