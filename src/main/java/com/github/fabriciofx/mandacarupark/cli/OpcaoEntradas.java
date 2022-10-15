9285dd0d454a767e2b8b4b1f47d327ed69ad1238
Fabrício Barros Cabral <fabriciofx@gmail.com>
2022-10-15 06:02:13 -0300

    (#168) Adiciona TextTable

--- src/main/java/com/github/fabriciofx/mandacarupark/cli/OpcaoEntradas.java
+++ src/main/java/com/github/fabriciofx/mandacarupark/cli/OpcaoEntradas.java
@@ -3,6 +3,9 @@ package com.github.fabriciofx.mandacarupark.cli;
 import com.github.fabriciofx.mandacarupark.Entrada;
 import com.github.fabriciofx.mandacarupark.Entradas;
 import com.github.fabriciofx.mandacarupark.console.Console;
+import com.github.fabriciofx.mandacarupark.text.TextTable;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class OpcaoEntradas implements Opcao {
     private final String mensagem;
@@ -31,15 +34,27 @@ public class OpcaoEntradas implements Opcao {
 
     @Override
     public void run() {
+        int linhas = 0;
         for (final Entrada entrada : this.entradas) {
-            this.console.write(
-                String.format(
-                    "%s %s %s\n",
-                    entrada.id(),
-                    entrada.sobre().get("placa").toString(),
-                    entrada.sobre().get("dataHora").toString()
-                )
-            );
+            linhas++;
         }
+        final String[][] tabela = new String[linhas][3];
+        int linha = 0;
+        for (final Entrada entrada : this.entradas) {
+            tabela[linha][0] = String.format(
+                " %s ",
+                entrada.id().numero()
+            );
+            tabela[linha][1] = String.format(
+                " %s ",
+                entrada.sobre().get("placa").toString()
+            );
+            tabela[linha][2] = String.format(
+                " %s ",
+                entrada.sobre().get("dataHora").toString()
+            );
+            linha++;
+        }
+        this.console.write(new TextTable(tabela).asString());
     }
 }
