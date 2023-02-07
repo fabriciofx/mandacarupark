f9f7868f8d041886f976f43a1b8f1d43d1531468
Fabrício Barros Cabral <fabriciofx@gmail.com>
2023-02-06 22:24:08 -0300

    (#265) Organiza o Footer

--- src/main/java/com/github/fabriciofx/mandacarupark/web/http/Footer.java
+++ src/main/java/com/github/fabriciofx/mandacarupark/web/http/Footer.java
@@ -17,45 +17,99 @@ public final class Footer<T> implements Text {
 
     @Override
     public String asString() {
-        final StringBuilder sb = new StringBuilder();
-        sb.append("<div class=\"center\">\n");
-        if (this.number - 1 >= 1) {
-            sb.append(
-                new Sprintf(
+        return new Sprintf(
+            "<div class=\"center\">\n%s%s%s\t\t</div>\n",
+            new Footer.Anterior(this.url, this.number).asString(),
+            new Footer.Numeros(
+                this.url, this.number, this.pages.count()
+            ).asString(),
+            new Footer.Proximo(
+                this.url, this.number, this.pages.count()
+            ).asString()
+        ).asString();
+    }
+
+    static final private class Anterior implements Text {
+        private final String url;
+        private final int number;
+
+        public Anterior(final String url, final int number) {
+            this.url = url;
+            this.number = number;
+        }
+
+        @Override
+        public String asString() {
+            final String content;
+            if (this.number - 1 >= 1) {
+                content = new Sprintf(
                     "\t\t  <a href=\"%s?page=%d\">< Anterior</a>\n",
                     this.url,
                     this.number - 1
-                ).asString()
-            );
-        } else {
-            sb.append("\t\t  < Anterior\n");
-        }
-        for (int num = 1; num <= this.pages.count(); num++) {
-            if (num == this.number) {
-                sb.append(num);
+                ).asString();
             } else {
-                sb.append(
-                    new Sprintf(
-                        "\t\t  <a href=\"%s?page=%d\">%d</a>\n",
-                        this.url,
-                        num,
-                        num
-                    ).asString()
-                );
+                content = "\t\t  < Anterior\n";
             }
+            return content;
         }
-        if (this.number + 1 <= this.pages.count()) {
-            sb.append(
-                new Sprintf(
+    }
+
+    static final private class Numeros implements Text {
+        private final String url;
+        private final int number;
+        private final int total;
+
+        public Numeros(final String url, final int number, final int total) {
+            this.url = url;
+            this.number = number;
+            this.total = total;
+        }
+
+        @Override
+        public String asString() {
+            final StringBuilder content = new StringBuilder();
+            for (int num = 1; num <= this.total; num++) {
+                if (num == this.number) {
+                    content.append(num);
+                } else {
+                    content.append(
+                        new Sprintf(
+                            "\t\t  <a href=\"%s?page=%d\">%d</a>\n",
+                            this.url,
+                            num,
+                            num
+                        ).asString()
+                    );
+                }
+            }
+            return content.toString();
+        }
+    }
+
+    static final private class Proximo implements Text {
+        private final String url;
+        private final int number;
+        private final int total;
+
+        public Proximo(final String url, final int number, final int total) {
+            this.url = url;
+            this.number = number;
+            this.total = total;
+        }
+
+        @Override
+        public String asString() {
+            final String content;
+            if (this.number + 1 <= this.total) {
+                content = new Sprintf(
                     "\t\t  <a href=\"%s?page=%d\">Próximo ></a>\n",
                     this.url,
                     this.number + 1
-                ).asString()
-            );
-        } else {
-            sb.append("\t\t  Próximo >\n");
+                ).asString();
+            } else {
+                content = "\t\t  Próximo >\n";
+            }
+            return content;
         }
-        sb.append("\t\t</div>\n");
-        return sb.toString();
     }
 }
